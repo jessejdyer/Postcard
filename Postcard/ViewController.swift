c84a8db08e0ac16b0e8194b5ac8b7aaefab825c9
Jesse Dyer <jessejdyer@alumni.uidaho.edu>
2014-10-13 19:58:59 -0400

    Added a comment to test commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // Mail Send Process
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.greenColor()
